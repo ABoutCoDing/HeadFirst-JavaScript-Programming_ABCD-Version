d870d02e4323f6fe6baadfd8460211cdd952d9dc
jamie <jamie@snowkiwi.com>
2017-03-04 13:56:36 +0900

    commit

--- Chapter2/battleship.js
+++ Chapter2/battleship.js
@@ -1,12 +1,12 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+//var location1 = 3;
+//var location2 = 4;
+//var location3 = 5;
 
 // 107 page
-//var randomroc = Math.floor(Math.random() * 5);
-//var location1 = randomroc;
-//var location2 = location1 + 1;
-//var location3 = location2 + 1;
+var randomroc = Math.floor(Math.random() * 5);
+var location1 = randomroc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 
 var guess;
 var hits = 0;
