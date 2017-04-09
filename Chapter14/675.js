649275e6f7ae1e633972fc9db689d0b892df90df
jamie han <jamie@jamieui-MacBook-Pro.local>
2017-04-09 18:56:43 +0900

    commit

--- Chapter14/675.js
+++ Chapter14/675.js
@@ -3,4 +3,4 @@ http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("헬로 월드~");
   response.end();
-}).listen(8888);
+}).listen(8888);  // http://127.0.0.1:8888
