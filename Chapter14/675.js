9f0b5b97c71914fdf510cca2de8c6725029a4797
jamie han <jamie@snowkiwi.com>
2017-05-14 23:22:30 +0900

    utf-8 추가

--- Chapter14/675.js
+++ Chapter14/675.js
@@ -1,6 +1,7 @@
 var http = require('http');
 http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.writeHead(200, {"Content-Type": "text/plain; charset=UTF-8"});
+  response.addHeader("Access-Control-Allow-Origin", "*");
   response.write("헬로 월드~");
   response.end();
 }).listen(8888);  // http://127.0.0.1:8888
